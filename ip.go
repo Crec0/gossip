431a6a5bba84fa408f4cec3627b9c2c4bc200f93
Crec0 <83436716+Crec0@users.noreply.github.com>
2023-03-19 00:21:34 -0700

    Look at various headers before x-forwarded-for

--- ip.go
+++ ip.go
@@ -21,6 +21,13 @@ var headersInOrder = []string{
 }
 
 func findIP(r *http.Request) string {
+	for _, header := range headersInOrder {
+		if ip := r.Header.Get(header); ip != "" {
+			log.Printf("Found ip %s in header %s", ip, header)
+			return ip
+		}
+	}
+
 	if ip := r.Header.Get("x-forwarded-for"); ip != "" {
 		candidates := strings.Split(ip, ",")
 		if len(candidates) > 0 {
@@ -28,13 +35,6 @@ func findIP(r *http.Request) string {
 		}
 		log.Printf("Found ip %s in header X-Forwarded-For", ip)
 		return ip
-	}
-
-	for _, header := range headersInOrder {
-		if ip := r.Header.Get(header); ip != "" {
-			log.Printf("Found ip %s in header %s", ip, header)
-			return ip
-		}
 	}
 
 	log.Printf("Fallback to remote header ip %s", r.RemoteAddr)
